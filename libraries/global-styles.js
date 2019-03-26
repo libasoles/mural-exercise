231075148416ac1fec31d963757adc02dafa93b0
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2019-03-26 10:37:49 -0300

    Add assets

--- libraries/global-styles.js
+++ libraries/global-styles.js
@@ -1,16 +1,30 @@
 import { createGlobalStyle, css } from 'styled-components';
 
+const fontFace = (family, style, weight, filename) => css`
+  @font-face {
+    font-family: '${family}';
+    font-style: ${style};
+    font-weight: ${weight};
+    font-display: fallback;
+    src: url(/static/fonts/${filename}.woff2) format('woff2');
+    }
+`;
+
 const baseStyles = css`
   width: 100%;
   height: 100%;
   margin: 0;
   padding: 0;
   border: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-    'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: 'Proxima Nova', -apple-system, 'Segoe UI', 'Helvetica Neue', sans-serif;
 `;
 
 const Baseline = createGlobalStyle`
+  ${fontFace('Proxima Nova', 'light', 300, 'ProximaNova-Light')}
+  ${fontFace('Proxima Nova', 'normal', 400, 'ProximaNova-Regular')}
+  ${fontFace('Proxima Nova', 'normal', 600, 'ProximaNova-Semibold')}
+  ${fontFace('Proxima Nova', 'italic', 700, 'ProximaNova-Bold')}
+
   html,
   body,
   :root {
