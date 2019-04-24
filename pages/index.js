18dc6327fceabc3c74174183e4ec3031d612596b
Guillermo Perez <gperez78@gmail.com>
2019-04-24 14:32:21 -0300

    Service worker draft

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import BasicLayout from '../components/Layouts';
 import { P, Wrapper } from '../components/Theme';
 import Hero from '../components/Hero';
+import useServiceWorker from '../libraries/serviceWorker/useServiceWorker';
 
-export default () => (
-  <BasicLayout title="MURAL">
-    <Hero
-      title="Think and collaborate visually. Anywhere, anytime."
-      text="Digital workspaces for visual collaboration, inspiration and innovation anytime, anywhere, on any device"
-    />
+const Home = ({ resources }) => {
+  useServiceWorker(resources);
+
+  return (
+    <BasicLayout title="MURAL">
+      <Hero
+        title="Think and collaborate visually. Anywhere, anytime."
+        text="Digital workspaces for visual collaboration, inspiration and innovation anytime, anywhere, on any device"
+      />
 
     <Wrapper>
       <P textAlign="center" fontSize={[0, 1]} color="#f06" mb={[2, 4]}>
@@ -16,8 +20,23 @@ export default () => (
       </P>
     </Wrapper>
 
-    <Wrapper>
-      <img src="https://mural.co/static/videos/home.jpg" alt="MURAL" />
-    </Wrapper>
-  </BasicLayout>
-);
+      <Wrapper>
+        <img src="https://mural.co/static/videos/home.jpg" alt="MURAL" />
+      </Wrapper>
+    </BasicLayout>
+  );
+};
+
+Home.propTypes = {
+  resources: PropTypes.array
+};
+
+Home.getInitialProps = async () => {
+  if (!process.browser) {
+    /* eslint global-require: 1 */
+    const resources = require('../libraries/serviceWorker/resourcesList').default;
+    return resources;
+  }
+};
+
+export default Home;
